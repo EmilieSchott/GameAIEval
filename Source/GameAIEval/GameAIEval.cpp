69e931ff34776fedf134e35c79e45a5074dc746a
Emilie Schott <emilie.schott@gmail.com>
2026-04-03 08:21:34 +0200

    Initial commit

---
+++ Source/GameAIEval/GameAIEval.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GameAIEval.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GameAIEval, "GameAIEval" );
